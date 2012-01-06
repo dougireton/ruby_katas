628defe885274c2914a29fb92766ba26e524b0d2
Doug Ireton <dougireton@gmail.com>
2012-01-06 07:39:10 -0600

    Add string multiply and hyphenate katas

--- lib/ruby_katas/01_hello_spec.rb
+++ lib/ruby_katas/01_hello_spec.rb
@@ -13,20 +13,20 @@
 # limitations under the License.
 
 
-__ = ""
+__ = ''
 
 # Hello, world in Ruby
 #
 # @return [String] Hello, world!
 def hello_world
 
-  # Change the double umnderscore so this method returns "Hello, world!"
-  return "Hello, __"
+  # Change the double umnderscore so this method returns 'Hello, world!'
+  return 'Hello, __'
 end
 
-describe "#hello_world" do
+describe 'hello_world' do
   it "retuns 'Hello, world!'" do
-    hello_world.should eq("Hello, world!")
+    hello_world.should eq('Hello, world!')
   end
 end
 
@@ -34,7 +34,7 @@ end
 # Hello, <name>
 #
 # @param [string] name of the person you want to say hello to
-# @return [string] "Hello <name>"
+# @return [string] 'Hello <name>'
 def hello_name(name)
 
   # Replace the blank lines between the braces with the method parameter 'name'
@@ -42,9 +42,9 @@ def hello_name(name)
   return "Hello #{ }"
 end
 
-describe "#hello_name" do
+describe 'hello_name' do
   it "says 'Hello <name>'" do
-    hello_name("Doug").should eq("Hello Doug")
+    hello_name('Doug').should eq('Hello Doug')
   end
 end
 
@@ -56,12 +56,11 @@ end
 def string_reverse(string)
 
   # see http://ruby-doc.org/core-1.9.3/String.html#method-i-reverse
-
 end
 
-describe "#string_reverse" do
-  it "reverses a string" do
-    string_reverse("stressed").should eq("desserts")
+describe 'string_reverse' do
+  it 'reverses a string' do
+    string_reverse('stressed').should eq('desserts')
   end
 end
 
@@ -73,19 +72,104 @@ end
 def string_multiply(string, n)
 
   # see http://www.ruby-doc.org/core-1.9.3/String.html#method-i-2A
+end
+
+describe 'string_multiply' do
+  it 'returns a new string with n copies concatenated together' do
+    string_multiply('A', 5).should eq('AAAAA')
+  end
+
+  it 'returns an empty string when n == 0' do
+    string_multiply('A', 0).should eq('')
+  end
+
+  it 'throws an ArgumentError when n < 0' do
+    expect { string_multiply('A', -1 ) }.to raise_error(ArgumentError)
+  end
+
+  it 'thows a TypeError when n is not a number' do
+    expect { string_multiply('A', 'bar') }.to raise_error(TypeError)
+  end
+end
+
+
+# Hyphenate a string
+#
+# @param [string] string to hyphenzate
+# @return [string] a new hyphenated string
+#
+# @see http://rubydoc.info/gems/activesupport/3.1.3/ActiveSupport/Inflector:parameterize
+#   for a more complete imeplmentation in Rails ActiveSupport
+def hyphenate(string_to_hyphenate)
+
+  # You will need to use several methods in Ruby's String class
+  # to accomplish your goal. See http://www.ruby-doc.org/core-1.9.3/String.html
+  # You will need to do the following:
+  # Strip leading and trailing whitespace
+  # Replace multiple spaces in the middle of a string with
+  # a single hyphen
+  #
+  # You can do it all on one line by chaining methods together
+  # like this: string_to_hyphenate.foo.bar(' ').baz
+
 
 end
 
-describe "#string_multiply" do
-  it "returns a new string with n copies concatenated together" do
-    string_multiply("A", 5).should eq("AAAAA")
+describe 'hyphenate' do
+
+  it 'hyphenates and downcases a string' do
+    hyphenate('  My dog has   fleas  ').should eq('my-dog-has-fleas')
   end
 
-  it "returns an empty string when n == 0" do
-    string_multiply("A", 0).should eq("")
+  it "doesn't hyphenate single words" do
+    hyphenate('single').should eq('single')
   end
 
-  it "throws an ArgumentError when n < 0" do
-    expect { string_multiply( A, -1 )}.to raise_error(ArgumentError)
+  it "doesn't hyphenate single words with whitespace" do
+    hyphenate('  single ').should eq('single')
+  end
+
+  it 'returns an empty string if you pass in a string containing only spaces' do
+    hyphenate('   ').should eq('')
+  end
+end
+
+
+# Open the Ruby built-in String class and add the hyphenate method
+# Since Ruby has open classes, you can add methods even to built-in classes
+#
+class String
+
+  # @param [string] string to hyphenzate
+  # @return [string] a new hyphenated string
+  def hyphenate
+
+    # Notice that the hyphenate method doesn't use any parameters
+    # The string you are trying to hyphenate will be passed as "self"
+    # Therefore, instead of string_to_hyphenate.foo.bar(' ').baz,
+    # you would type this: self.foo.bar(' ').baz
+    #
+    # Then you would call your custom String instance method like this:
+    # 'My string'.hyphenate
+
+  end
+end
+
+describe String, '#hyphenate' do
+
+  it 'hyphenates and downcases a string' do
+    '  My dog has   fleas  '.hyphenate.should eq('my-dog-has-fleas')
+  end
+
+  it "doesn't hyphenate single words" do
+    'single'.hyphenate.should eq('single')
+  end
+
+  it "doesn't hyphenate single words with whitespace" do
+    '  single '.hyphenate.should eq('single')
+  end
+
+  it 'returns an empty string if you pass in a string containing only spaces' do
+    '   '.hyphenate.should eq('')
   end
 end
